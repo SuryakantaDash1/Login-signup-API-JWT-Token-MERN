a8c51a23c28d6363ec72b2914af048ebd7174371
Suryakanta Dash <sdash8273@gmail.com>
2023-03-05 23:20:30 +0530

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,54 @@
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import './App.css';
+/** import all components */
+import Username from './components/Username';
+import Password from './components/Password';
+import Register from './components/Register';
+import Profile from './components/Profile';
+import Recovery from './components/Recovery';
+import Reset from './components/Reset';
+import PageNotFound from './components/PageNotFound';
+
+
+/** root routes */
+const router = createBrowserRouter([
+    {
+        path:'/',
+        element: <Username></Username>
+    },
+    {
+        path:'/register',
+        element: <Register></Register>
+    },
+    {
+        path:'/password',
+        element: <Password></Password>
+    },
+    {
+        path:'/profile',
+        element: <Profile></Profile>
+    },
+    {
+        path:'/recovery',
+        element: <Recovery></Recovery>
+    },
+    {
+        path:'/reset',
+        element: <Reset></Reset>
+    },
+    {
+        path:'*',
+        element: <PageNotFound></PageNotFound>
+    }
+])
 
 function App() {
   return (
-   <>
-   </>
-  );
+    <main>
+       <RouterProvider router={router}></RouterProvider>
+    </main>
+  )
 }
 
-export default App;
+export default App
