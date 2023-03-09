88c053b5502429c943feae512bcd95e49c320499
Suryakanta Dash <sdash8273@gmail.com>
2023-03-09 12:16:24 +0530

    updated

--- client/src/components/Username.js
+++ client/src/components/Username.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import profile from '../assets/profile.png';
+import { Toaster } from 'react-hot-toast';
+import { useFormik } from 'formik';
 
 import styles from '../styles/Username.module.css';
 
 function Username() {
+
+    const formik = useFormik({
+        initialValues : {
+            username: ''
+        },
+        validate : 
+        validateOnBlur : false,
+        validateOnChange : false,
+        onSubmit : async values => {
+            console.log(values)
+        }
+    })
+
   return (
     <div className="container mx-auto">
         <div className='flex justify-center items-center h-screen'>
@@ -16,13 +31,13 @@ function Username() {
                     </span>
                 </div>
 
-                <form className='py-1'>
+                <form className='py-1' onSubmit={formik.handleSubmit}>
                     <div className='profile flex justify-center py-4'>
                         <img src={profile} className={styles.profile_img} alt="profile" />
                     </div>
 
                     <div className='textbox flex flex-col items-center gap-6'>
-                        <input className={styles.textbox} type="text" placeholder='Username' />
+                        <input {...formik.getFieldProps('username')} className={styles.textbox} type="text" placeholder='Username' />
                         <button className={styles.btn} type='submit'>Let's Go</button>
                     </div>
 
