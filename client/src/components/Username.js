63749020d16c0b4144d0116fb2e85bfab6b6f4c5
Suryakanta Dash <sdash8273@gmail.com>
2023-03-09 13:28:08 +0530

    updated

--- client/src/components/Username.js
+++ client/src/components/Username.js
@@ -24,7 +24,7 @@ function Username() {
   return (
     <div className="container mx-auto">
 
-        <Toaster position='top-center' reverseOrder={false}></Toaster>
+        <Toaster position='top-center' reverseOrder={false}> </Toaster>
 
         <div className='flex justify-center items-center h-screen'>
             <div className={styles.glass}>
