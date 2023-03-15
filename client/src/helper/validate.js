8c41529bfdf2f1c74ab6235735cea968aee62f70
Suryakanta Dash <sdash8273@gmail.com>
2023-03-15 16:31:29 +0530

    updated

--- client/src/helper/validate.js
+++ client/src/helper/validate.js
@@ -8,6 +8,33 @@ export async function usernameValidate(values) {
     return errors; 
 }
 
+// validate password 
+
+export async function passwordValidate(values){
+    const errors = passwordVerify({}, values);
+
+    return errors;
+}
+
+// validate Password 
+
+function passwordVerify(errors = {}, values){
+    const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/; 
+    if(!values.password){
+        errors.password = toast.error("Password Required");
+    } else if(values.password.includes(" ")) {
+        errors.password = toast.error(" Wrong password");
+    } else if(values.password.length < 4){
+        errors.password = toast.error("Password must be more than 4 character");
+    } else if(!specialChars.test(values.password)){
+        errors.password = toast.error("Passwords must have special character");
+    }
+
+    return errors;
+}
+
+
+
 // Validate Username 
 
 function usernameVerify(error ={}, values) {
